2fdb20198cf183058ff68c4c61b1cfa53c17d764
Shivaprasad Biradar <bshivaprasad004@gmail.com>
2023-02-14 15:57:40 +0530

    TextUtils Web APP

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import TextForm from './components/TextForm';
 // import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
- 
+
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
@@ -39,22 +39,22 @@ function App() {
   }
   return (
     <>
-    <Router>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    {/* <Router> */}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}  />
     <Alert alert={alert}/>
     <div className="container my-3">
-    <Routes>
+    {/* <Routes> */}
     {/* /users --> Component 1
         /users/home --> Component 2 */}
-          <Route exact path="/about" mode={mode}/>
+          {/* <Route exact path="/about" mode={mode}/> */}
             {/* <About  /> */}
           {/* </Route> */}
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>}/>
-            
+          {/* <Route exact path="/"/> */}
+            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
           {/* </Route> */}
-    </Routes>
+    {/* </Routes> */}
     </div>
-    </Router>
+    {/* </Router> */}
     </> 
   );
 }
